b82728a4e07da08d6c5087ca237fe54948c6f09f
Joseph Wayne Norton <norton@alum.mit.edu>
2011-01-14 14:53:50 +0900

    initial clus-hibari.sh script

--- priv/clus-hibari.sh
+++ priv/clus-hibari.sh
@@ -28,105 +28,398 @@ fi
 # main function
 # ----------------------------------------------------------------------
 main() {
-        env_sanity "$@"
-        args_sanity "$@"
+    env_sanity "$@"
+    args_sanity "$@"
 
-        if [ "$CMD" = "init" ] ; then
-            init
-        else
-            usage;
-        fi
-    }
+    if [ "$CMD" = "init" ] ; then
+        init
+    elif [ "$CMD" = "start" ] ; then
+        start
+    elif [ "$CMD" = "bootstrap" ] ; then
+        bootstrap
+    elif [ "$CMD" = "stop" ] ; then
+        stop
+    else
+        usage;
+    fi
+}
 
 
 # ----------------------------------------------------------------------
 # helper functions
 # ----------------------------------------------------------------------
-    die() {
-        cat <<EOF
+die() {
+    cat <<EOF
 $@
 
 run $0 without any arguments for usage
 EOF
-        exit 1
-    }
+    exit 1
+}
 
-    usage() {
-        cat <<EOF
+usage() {
+    cat <<EOF
 
 Usage: $0 [-f] <command> ...
 
   <command> is one of the following:
-    init <erlang_dir> <user> <host> ...
+    init <user> <hibari.cfg>
+    start <user> <hibari.cfg>
+    bootstrap <user> <hibari.cfg>
+    stop <user> <hibari.cfg>
 
   - "-f" is enable force mode.  disable safety checks to prevent
      deleting and/or overwriting an existing installation.
-  - <erlang_dir> is that root director for erlang
-  - <user> is the account on the server where you will be installing
-    Hibari
-  - <host> is server's name
+  - <user> is the account on the server(s) where you will be
+    installing Hibari
+  - <hibari.cfg> is Hibari's cluster config file
 
 
 Example usage:
-  $0 init /usr/local/lib/erlang hibari `hostname`
+  $0 init hibari hibari.cfg
 
 Notes:
-  - <user> must be simple names -- no special characters etc please
-    (only alphanumerics, dot, hyphen, underscore)
-
+  - <user> and hosts in the cluster config file must be simple names
+    -- no special characters etc please (only alphanumerics, dot,
+    hyphen, underscore)
 EOF
-        exit 1;
-    }
+    exit 1;
+}
 
 
 # ----------------------------------------------------------------------
 # environment checks
 # ----------------------------------------------------------------------
-    env_sanity() {
-        local PERL=($PERL)
-        local SSH=($SSH)
+env_sanity() {
+    local PERL=($PERL)
+    local SSH=($SSH)
 
-        for i in ${PERL[0]} ${SSH[0]} ; do
-            `test -f $i &> $NULLFILE` || `which $i &> $NULLFILE` ||
-            die "$i does not exist or not found in ($PATH)"
-        done
-    }
+    for i in ${PERL[0]} ${SSH[0]} ; do
+        `test -f $i &> $NULLFILE` || `which $i &> $NULLFILE` ||
+        die "$i does not exist or not found in ($PATH)"
+    done
+}
 
 
 # ----------------------------------------------------------------------
 # arguments checks
 # ----------------------------------------------------------------------
-    args_sanity() {
-        FORCE=
-        if [ "$1" = "-f" ] ; then
-            FORCE=-f
-            shift
-        fi
-
-        if [[ -z $1 || -z $2 || -z $3 || -z $4 ]] ; then
-            usage
-        fi
-
-        CMD=$1
-        ERLANG_DIR=$2
-        NODE_USER=$3
+args_sanity() {
+    FORCE=
+    if [ "$1" = "-f" ] ; then
+        FORCE=-f
         shift
-        shift
-        shift
-        NODE_HOSTS=($*)
+    fi
 
-        echo $NODE_USER | $PERL -lne 'exit 1 if /[^a-zA-Z0-9._-]/' || \
-            die "user '$NODE_USER' invalid"
-        if [ "$NODE_USER" = "root" ] ; then
-            die "user '$NODE_USER' unsupported"
-        fi
-    }
+    if [[ -z $1 || -z $2 || -z $3 ]] ; then
+        usage
+    fi
+
+    CMD=$1
+    NODE_USER=$2
+    CONFIG_FILE=$3
+
+    echo $NODE_USER | $PERL -lne 'exit 1 if /[^a-zA-Z0-9._-]/' || \
+        die "user '$NODE_USER' invalid"
+    if [ "$NODE_USER" = "root" ] ; then
+        die "user '$NODE_USER' unsupported"
+    fi
+
+    source $CONFIG_FILE || \
+        die "source '$CONFIG_FILE' failed"
+
+    for i in $ADMIN_NODES $BRICK_NODES $ALL_NODES ; do
+        echo $i | $PERL -lne 'exit 1 if /[^a-zA-Z0-9._-]/' || \
+            die "host '$i' invalid"
+    done
+
+    if [ ${#ALL_NODES[@]} -lt 1 ] ; then
+        die "missing ALL_NODES in '$CONFIG_FILE'"
+    fi
+    if [ ${#ADMIN_NODES[@]} -lt 1 ] ; then
+        die "missing ADMIN_NODES in '$CONFIG_FILE'"
+    fi
+    if [ ${#BRICK_NODES[@]} -lt 1 ] ; then
+        die "missing BRICK_NODES in '$CONFIG_FILE'"
+    fi
+    if [ -z $BRICKS_PER_CHAIN ] ; then
+        die "missing BRICKS_PER_CHAIN in '$CONFIG_FILE'"
+    fi
+    if [ $BRICKS_PER_CHAIN -lt 1 ] ; then
+        die "invalid BRICKS_PER_CHAIN in '$CONFIG_FILE'"
+    fi
+    if [ ${#ALL_NETA_ADDRS[@]} -lt 1 ] ; then
+        die "missing ALL_NETA_ADDRS in '$CONFIG_FILE'"
+    fi
+    if [ ${#ALL_NETB_ADDRS[@]} -lt 1 ] ; then
+        die "missing ALL_NETB_ADDRS in '$CONFIG_FILE'"
+    fi
+    if [ -z $ALL_NETA_BCAST ] ; then
+        die "missing ALL_NETA_BCAST in '$CONFIG_FILE'"
+    fi
+    if [ -z $ALL_NETB_BCAST ] ; then
+        die "missing ALL_NETB_BCAST in '$CONFIG_FILE'"
+    fi
+    if [ -z $ALL_NETA_TIEBREAKER ] ; then
+        die "missing ALL_NETA_TIEBREAKER in '$CONFIG_FILE'"
+    fi
+    if [ -z $ALL_HEART_UDP_PORT ] ; then
+        die "missing ALL_HEART_UDP_PORT in '$CONFIG_FILE'"
+    fi
+    if [ -z $ALL_HEART_XMIT_UDP_PORT ] ; then
+        die "missing ALL_HEART_XMIT_UDP_PORT in '$CONFIG_FILE'"
+    fi
+
+    if [ ${#ALL_NODES[@]} -lt ${#ADMIN_NODES[@]} ] ; then
+        die "ALL_NODES is less than ADMIN_NODES"
+    fi
+    if [ ${#ADMIN_NODES[@]} -eq 1 ] ; then
+        true
+    elif [ ${#ADMIN_NODES[@]} -eq 2 ] ; then
+        die "2 ADMIN_NODES is not supported"
+    fi
+    if [ ${#ALL_NODES[@]} -lt ${#BRICK_NODES[@]} ] ; then
+        die "ALL_NODES is less than BRICK_NODES"
+    fi
+    if [ ${#ALL_NODES[@]} -ne ${#ALL_NETA_ADDRS[@]} ] ; then
+        die "nonequal # of ALL_NODES and ALL_NETA_ADDRS"
+    fi
+    if [ ${#ALL_NODES[@]} -ne ${#ALL_NETB_ADDRS[@]} ] ; then
+        die "nonequal # of ALL_NODES and ALL_NETB_ADDRS"
+    fi
+}
 
 
 # ----------------------------------------------------------------------
 # init
 # ----------------------------------------------------------------------
-    init() {
-        echo $ERLANG_DIR $NODE_USER ${NODE_HOSTS[@]}
-    }
+init() {
+    # download Hibari
+    if [ ! -d  hibari ] ; then
+        mkdir hibari
+        pushd hibari
+        repo init -u git://github.com/hibari/manifests.git -m hibari-default.xml
+        repo sync
+        popd
+    fi
 
+    local N0=${#ALL_NODES[@]}
+    local N=$(($N0 - 1))
+
+    # generate cookie
+    local COOKIE=`randcookie`
+    if [ -z $COOKIE ] ; then
+        die "node $NODE generate cookie failed"
+    fi
+
+    # setup NODE
+    for I in `seq 0 $N`; do
+        (
+            local NODE=${ALL_NODES[$I]}
+            local ADMIN_NODES_MINUSMOI=(`echo ${ADMIN_NODES[@]} | xargs -n 1 echo | grep -v $NODE | xargs echo`)
+
+            local CS_NODE=(`hibari_nodes_cs $NODE`)
+            local CS_ALL_NODES=(`hibari_nodes_cs ${ALL_NODES[@]}`)
+            local CS_ADMIN_NODES=(`hibari_nodes_cs ${ADMIN_NODES[@]}`)
+            local CS_ADMIN_NODES_MINUSMOI=(`hibari_nodes_cs ${ADMIN_NODES_MINUSMOI[@]}`)
+
+            # append Erlang Dir to path
+            $SSH $NODE_USER@$NODE "sed -i.bak '\$a\export PATH=$ERLANG_DIR/bin:\$PATH' .bashrc" || \
+                die "node $NODE setup .bashrc failed"
+
+            # stop Hibari
+            $SSH $NODE_USER@$NODE "(cd hibari/hibari &> $NULLFILE; ./tmp/hibari/bin/hibari stop &> $NULLFILE) || true" || \
+                die "node $NODE stop failed"
+            # kill all Hibari beam.smp processes
+            $SSH $NODE_USER@$NODE "pkill -9 -u $NODE_USER beam.smp || true" || \
+                die "node $NODE pkill beam.smp failed"
+
+            # rsync Hibari
+            rsync -a hibari/ $NODE_USER@$NODE:hibari/ || \
+                die "node $NODE rsync failed"
+
+            # make Hibari package
+            $SSH $NODE_USER@$NODE "make -C hibari/hibari package &> $NULLFILE" || \
+                die "node $NODE make failed"
+            # untar Hibari package
+            $SSH $NODE_USER@$NODE "rm -rf hibari/hibari/tmp; mkdir hibari/hibari/tmp; tar -C hibari/hibari/tmp -xzf hibari/*.tgz" || \
+                die "node $NODE untar failed"
+
+            # configure Hibari package
+            $SSH $NODE_USER@$NODE "sed -i.bak \
+-e 's/-sname .*/-sname $CS_NODE/' \
+-e 's/-name .*/-sname $CS_NODE/' \
+-e 's/-setcookie .*/-setcookie $COOKIE/' \
+hibari/hibari/tmp/hibari/etc/vm.args" || \
+                die "node $NODE vm.args setup failed"
+
+            $SSH $NODE_USER@$NODE "sed -i.bak \
+-e 's/{distributed, \[{gdss_admin, \(.*\), .*}\]}/{distributed, \[{gdss_admin, \1, [$CS_ADMIN_NODES]}\]}/' \
+-e 's/{sync_nodes_optional,.*}/{sync_nodes_optional, [$CS_ADMIN_NODES_MINUSMOI]}/' \
+-e 's/{admin_server_distributed_nodes,.*}/{admin_server_distributed_nodes, [$CS_ADMIN_NODES]}/' \
+-e 's/{network_a_address,.*}/{network_a_address, \"${ALL_NETA_ADDRS[$I]}\"}/' \
+-e 's/{network_b_address,.*}/{network_b_address, \"${ALL_NETB_ADDRS[$I]}\"}/' \
+-e 's/{network_a_broadcast_address,.*}/{network_a_broadcast_address, \"$ALL_NETA_BCAST\"}/' \
+-e 's/{network_b_broadcast_address,.*}/{network_b_broadcast_address, \"$ALL_NETB_BCAST\"}/' \
+-e 's/{network_a_tiebreaker,.*}/{network_a_tiebreaker, \"$ALL_NETA_TIEBREAKER\"}/' \
+-e 's/{network_monitor_enable,.*}/{network_monitor_enable, true}/' \
+-e 's/{network_monitor_monitored_nodes,.*}/{network_monitor_monitored_nodes, [$CS_ALL_NODES]}/' \
+-e 's/{heartbeat_status_udp_port,.*}/{heartbeat_status_udp_port, $ALL_HEART_UDP_PORT}/' \
+-e 's/{heartbeat_status_xmit_udp_port,.*}/{heartbeat_status_xmit_udp_port, $ALL_HEART_XMIT_UDP_PORT}/' \
+hibari/hibari/tmp/hibari/etc/app.config" || \
+                die "node $NODE app.config setup failed"
+
+            echo $NODE_USER@$NODE
+        ) &
+    done
+
+    wait
+}
+
+randcookie() {
+    echo `(cat /dev/urandom | strings | tr -c -d "a-zA-Z0-9-_" | fold -w 25 | head -1) 2> $NULLFILE`
+}
+
+
+# ----------------------------------------------------------------------
+# start
+# ----------------------------------------------------------------------
+start() {
+    local N0=${#ALL_NODES[@]}
+    local N=$(($N0 - 1))
+
+    # start NODE
+    for I in `seq 0 $N`; do
+        (
+            local NODE=${ALL_NODES[$I]}
+
+            # start Hibari package
+            $SSH $NODE_USER@$NODE "cd hibari/hibari; ./tmp/hibari/bin/hibari start" || \
+                die "node $NODE start failed"
+
+            echo $NODE_USER@$NODE
+        ) &
+    done
+
+    wait
+}
+
+
+# ----------------------------------------------------------------------
+# bootstrap
+# ----------------------------------------------------------------------
+bootstrap() {
+    local NODE=${ADMIN_NODES[0]}
+    local WS_BRICK_NODES=`hibari_nodes_ws ${BRICK_NODES[@]}`
+
+    # bootstrap Hibari package
+    $SSH $NODE_USER@$NODE "cd hibari/hibari; ./tmp/hibari/bin/hibari-admin bootstrap $BRICKS_PER_CHAIN $WS_BRICK_NODES" || \
+        die "node $NODE bootstrap failed"
+
+    echo "$NODE_USER@$NODE => $WS_BRICK_NODES"
+}
+
+
+# ----------------------------------------------------------------------
+# stop
+# ----------------------------------------------------------------------
+stop() {
+    local N0=${#ALL_NODES[@]}
+    local N=$(($N0 - 1))
+
+    # stop NODE
+    for I in `seq 0 $N`; do
+        (
+            local NODE=${ALL_NODES[$I]}
+
+            # stop Hibari package
+            $SSH $NODE_USER@$NODE "cd hibari/hibari; ./tmp/hibari/bin/hibari stop" || \
+                die "node $NODE stop failed"
+
+            echo $NODE_USER@$NODE
+        ) &
+    done
+
+    wait
+}
+
+
+# ----------------------------------------------------------------------
+# hibari_nodes_cs
+# ----------------------------------------------------------------------
+hibari_nodes_cs() {
+    local NODES=($@)
+    local N0=${#NODES[@]}
+
+    if [ $N0 -gt 0 ] ; then
+        local N=$(($N0 - 1))
+        local nodes=""
+
+        for X in `seq 0 $N`; do
+            local node=`echo ${NODES[$X]} | sed "s/'//g"`
+            nodes="$nodes,hibari@$node"
+        done
+        echo `echo $nodes | sed 's/^,//'`
+    else
+        echo
+    fi
+}
+
+
+# ----------------------------------------------------------------------
+# hibari_nodes_ws
+# ----------------------------------------------------------------------
+hibari_nodes_ws() {
+    local NODES=($@)
+    local N0=${#NODES[@]}
+
+    if [ $N0 -gt 0 ] ; then
+        local N=$(($N0 - 1))
+        local nodes=""
+
+        for X in `seq 0 $N`; do
+            local node=`echo ${NODES[$X]} | sed "s/'//g"`
+            nodes="$nodes hibari@$node"
+        done
+        echo `echo $nodes | sed 's/^ //'`
+    else
+        echo
+    fi
+}
+
+
+# ----------------------------------------------------------------------
+# is_int
+# ----------------------------------------------------------------------
+is_int() {
+    return $(test "$@" -eq "$@" >/dev/null 2>&1);
+}
+
+
+# ----------------------------------------------------------------------
+# seq
+# ----------------------------------------------------------------------
+seq() {
+    declare incr n1 n2 num1 num2
+    if [[ $# -eq 1 ]]; then
+        if ! $(is_int "$1"); then echo 'No integer!'; return 1; fi
+        for ((i=1; i<=${1}; i++)) { printf "%d\n" ${i}; }
+    elif [[ $# -eq 2 ]]; then
+        if ! $(is_int "$1") || ! $(is_int "$2"); then echo 'Not all arguments are integers!'; return 1; fi
+
+        if [[ $1 -eq $2 ]]; then
+            echo $1
+        elif [[ $2 -gt $1 ]]; then
+            for ((i=${1}; i<=${2}; i++)) { printf "%d\n" ${i}; }
+        elif [[ $1 -gt $2 ]]; then
+            for ((i=${1}; i>=${2}; i--)) { printf "%d\n" ${i}; }
+        fi
+
+    elif [[ $# -eq 3 ]]; then
+        num1=${1}
+        incr=${2}
+        num2=${3}
+        /usr/bin/awk -v n1=${num1} -v n2=${num2} -v add=${incr} 'BEGIN{ for(i=n1; i<=n2; i+=add) print i;}' | /usr/bin/sed -E '/e/s/^.+e.+$/0/'
+    fi
+    return 0
+}
--- priv/clus.sh
+++ priv/clus.sh
@@ -39,152 +39,199 @@ fi
 # main function
 # ----------------------------------------------------------------------
 main() {
-        env_sanity "$@"
-        args_sanity "$@"
+    env_sanity "$@"
+    args_sanity "$@"
 
-        if [ "$CMD" = "init" ] ; then
-            init
-        else
-            usage;
-        fi
-    }
+    if [ "$CMD" = "init" ] ; then
+        init
+    elif [ "$CMD" = "delete" ] ; then
+        delete
+    else
+        usage;
+    fi
+}
 
 
 # ----------------------------------------------------------------------
 # helper functions
 # ----------------------------------------------------------------------
-    die() {
-        cat <<EOF
+die() {
+    cat <<EOF
 $@
 
 run $0 without any arguments for usage
 EOF
-        exit 1
-    }
+    exit 1
+}
 
-    usage() {
-        cat <<EOF
+usage() {
+    cat <<EOF
 
 Usage: $0 [-f] <command> ...
 
   <command> is one of the following:
-    init <user> <host> [<admin_name>]
+    init <user> <host> [<installer_user>]
+    delete <user> <host> [<installer_user>]
 
   - "-f" is enable force mode.  disable safety checks to prevent
      deleting and/or overwriting an existing installation.
   - <user> is the account on the server where you will be installing
-    Node
   - <host> is that server's hostname (or IP address)
-  - <admin_name> is *your* loginname
+  - <installer_user> is *your* loginname
 
 Example usage:
   $0 init skylark `hostname`
 
 Notes:
-  - <user> and <admin_name> must be simple names -- no special
-    characters etc please (only alphanumerics, dot, hyphen,
+  - <user>, <host>, and <installer_user> must be simple names -- no
+    special characters etc please (only alphanumerics, dot, hyphen,
     underscore)
-  - <admin_name> should be your name, for clarity, or whoever will be
-    the Node admin.  The default <admin_name> is "$USER"
+  - <user> must be different than <installer_user>.
+  - <installer_user> should be your name, for clarity, or whoever will
+    be the installer user.  The default <installer_user> is "$USER"
 
 EOF
-        exit 1;
-    }
+    exit 1;
+}
 
 
 # ----------------------------------------------------------------------
 # environment checks
 # ----------------------------------------------------------------------
-    env_sanity() {
-        local EXPECT=($EXPECT)
-        local GROUPADD=($GROUPADD)
-        local GROUPDEL=($GROUPDEL)
-        local PERL=($PERL)
-        local SSH=($SSH)
-        local SSHCOPYID=($SSHCOPYID)
-        local SUDO=($SUDO)
+env_sanity() {
+    local EXPECT=($EXPECT)
+    local PERL=($PERL)
+    local SSH=($SSH)
+    local SSHCOPYID=($SSHCOPYID)
+    local SUDO=($SUDO)
 
-        for i in ${EXPECT[0]} ${PERL[0]} ${SSH[0]} ${SSHCOPYID[0]} ${SUDO[0]} ; do
-            `test -f $i &> $NULLFILE` || `which $i &> $NULLFILE` ||
-            die "$i does not exist or not found in ($PATH)"
-        done
-    }
+    for i in ${EXPECT[0]} ${PERL[0]} ${SSH[0]} ${SSHCOPYID[0]} ${SUDO[0]} ; do
+        `test -f $i &> $NULLFILE` || `which $i &> $NULLFILE` ||
+        die "$i does not exist or not found in ($PATH)"
+    done
+}
 
 
 # ----------------------------------------------------------------------
 # arguments checks
 # ----------------------------------------------------------------------
-    args_sanity() {
-        FORCE=
-        if [ "$1" = "-f" ] ; then
-            FORCE=-f
-            shift
-        fi
+args_sanity() {
+    FORCE=
+    if [ "$1" = "-f" ] ; then
+        FORCE=-f
+        shift
+    fi
 
-        if [[ -z $1 || -z $2 || -z $3 ]] ; then
-            usage
-        fi
+    if [[ -z $1 || -z $2 || -z $3 ]] ; then
+        usage
+    fi
 
-        CMD=$1
-        NODE_USER=$2
-        NODE_HOST=$3
-        if [ -z $4 ] ; then
-            ADMIN_NAME=$USER
-        else
-            ADMIN_NAME=$4
-        fi
+    CMD=$1
+    NODE_USER=$2
+    NODE_HOST=$3
+    if [ -z $4 ] ; then
+        INSTALLER_USER=$USER
+    else
+        INSTALLER_USER=$4
+    fi
 
-        echo $NODE_USER | $PERL -lne 'exit 1 if /[^a-zA-Z0-9._-]/' || \
-            die "user '$NODE_USER' invalid"
-        [[ "$NODE_USER" = "root" ]] && \
-            die "user '$NODE_USER' unsupported"
-        echo $ADMIN_NAME | $PERL -lne 'exit 1 if /[^a-zA-Z0-9._-]/' || \
-            die "admin_name '$ADMIN_NAME' invalid"
-    }
+    echo $NODE_USER | $PERL -lne 'exit 1 if /[^a-zA-Z0-9._-]/' || \
+        die "user '$NODE_USER' invalid"
+    if [[ "$NODE_USER" = "root" || "$NODE_USER" = "$INSTALLER_USER" ]] ; then
+        die "user '$NODE_USER' unsupported"
+    fi
+    echo $NODE_HOST | $PERL -lne 'exit 1 if /[^a-zA-Z0-9._-]/' || \
+        die "user '$NODE_HOST' invalid"
+    echo $INSTALLER_USER | $PERL -lne 'exit 1 if /[^a-zA-Z0-9._-]/' || \
+        die "installer_user '$INSTALLER_USER' invalid"
+}
 
 
 # ----------------------------------------------------------------------
 # init
 # ----------------------------------------------------------------------
-    init() {
+init() {
     # check ssh w/ sudo
-        $SSH $NODE_HOST '$SUDO true' || \
-            die "ssh '$NODE_HOST' sudo failed"
+    $SSH $NODE_HOST '$SUDO true' || \
+        die "ssh '$NODE_HOST' sudo failed"
 
     # force check
-        if [ -z $FORCE ] ; then
-            $SSH $NODE_HOST "grep $Q '^$NODE_USER:' $PASSWDFILE" && \
-                die "remote user '$NODE_USER@$NODE_HOST' exists"
-            $SSH $NODE_HOST "grep $Q '^$NODE_USER:' $GROUPFILE" && \
-                die "remote group '$NODE_USER@$NODE_HOST' exists"
-        fi
+    if [ -z $FORCE ] ; then
+        $SSH $NODE_HOST "grep $Q '^$NODE_USER:' $PASSWDFILE" && \
+            die "remote user '$NODE_USER@$NODE_HOST' exists"
+        $SSH $NODE_HOST "grep $Q '^$NODE_USER:' $GROUPFILE" && \
+            die "remote group '$NODE_USER@$NODE_HOST' exists"
+    fi
+
+    # kill all old user processes
+    $SSH $NODE_HOST "$SUDO pkill -9 -u $NODE_USER &> $NULLFILE || true" || \
+        die "remote user '$NODE_USER@$NODE_HOST' kill failed"
 
     # delete old user and group
-        $SSH $NODE_HOST "grep $Q '^$NODE_USER:' $PASSWDFILE" && \
-            ($SSH $NODE_HOST "$SUDO $USERDEL -r $NODE_USER" || \
-            die "remote user '$NODE_USER@$NODE_HOST' del failed")
-        $SSH $NODE_HOST "grep $Q '^$NODE_USER:' $GROUPFILE" && \
-            ($SSH $NODE_HOST "$SUDO $GROUPDEL $NODE_USER" || \
-            die "remote group '$NODE_USER@$NODE_HOST' del failed")
+    $SSH $NODE_HOST "grep $Q '^$NODE_USER:' $PASSWDFILE" && \
+        ($SSH $NODE_HOST "$SUDO $USERDEL -r $NODE_USER" || \
+        die "remote user '$NODE_USER@$NODE_HOST' del failed")
+    $SSH $NODE_HOST "grep $Q '^$NODE_USER:' $GROUPFILE" && \
+        ($SSH $NODE_HOST "$SUDO $GROUPDEL $NODE_USER" || \
+        die "remote group '$NODE_USER@$NODE_HOST' del failed")
 
     # add new group and user
-        $SSH $NODE_HOST "$SUDO $GROUPADD -r $NODE_USER" || \
-            die "remote group '$NODE_USER@$NODE_HOST' add failed"
-        $SSH $NODE_HOST "$SUDO $USERADD -m -r -g $NODE_USER -d $HOMEBASEDIR/$NODE_USER -c '$ADMIN_NAME node' $NODE_USER" || \
-            die "remote user '$NODE_USER@$NODE_HOST' add failed"
+    $SSH $NODE_HOST "$SUDO $GROUPADD -r $NODE_USER" || \
+        die "remote group '$NODE_USER@$NODE_HOST' add failed"
+    $SSH $NODE_HOST "$SUDO $USERADD -m -r -g $NODE_USER -d $HOMEBASEDIR/$NODE_USER -c '$NODE_USER node' $NODE_USER" || \
+        die "remote user '$NODE_USER@$NODE_HOST' add failed"
 
     # copy ssh identity
-        local passwd=`cat /dev/urandom |tr -dc "a-zA-Z0-9-_" | fold -w 25 | head -1`
-        $SSH $NODE_HOST "echo $passwd | $SUDO passwd --stdin $NODE_USER &> $NULLFILE" || \
-            ie "user '$NODE_USER@$NODE_HOST' passwd failed"
-        $SSH $NODE_HOST "$SUDO $USERMOD -U $NODE_USER" || \
-            die "remote user '$NODE_USER@$NODE_HOST' mod failed"
-        { eval "$EXPECT -c 'spawn $SSHCOPYID $NODE_USER@$NODE_HOST' -c 'expect password:' -c 'send $passwd\n' -c 'expect eof' &> $NULLFILE"; } || \
-            die "remote user '$NODE_USER@$NODE_HOST' ssh-copy-id failed"
-        $SSH $NODE_HOST "$SUDO $USERMOD -L $NODE_USER" || \
-            die "remote user '$NODE_USER@$NODE_HOST' mod failed"
+    local passwd=`randpasswd`
+    if [ -z $passwd ] ; then
+        die "remote user '$NODE_USER@$NODE_HOST' passwd gen failed"
+    fi
+    $SSH $NODE_HOST "echo $passwd | $SUDO passwd --stdin $NODE_USER &> $NULLFILE" || \
+        die "user '$NODE_USER@$NODE_HOST' passwd failed"
+    $SSH $NODE_HOST "$SUDO $USERMOD -U $NODE_USER" || \
+        die "remote user '$NODE_USER@$NODE_HOST' mod failed"
+    { eval "$EXPECT -c 'spawn $SSHCOPYID $NODE_USER@$NODE_HOST' -c 'expect password:' -c 'send $passwd\n' -c 'expect eof' &> $NULLFILE"; } || \
+        die "remote user '$NODE_USER@$NODE_HOST' ssh-copy-id failed"
+    $SSH $NODE_HOST "$SUDO $USERMOD -L $NODE_USER" || \
+        die "remote user '$NODE_USER@$NODE_HOST' mod failed"
 
     # check ssh
-        $SSH $NODE_USER@$NODE_HOST 'echo `whoami`@`hostname`' || \
-            die "remote ssh '$NODE_USER@$NODE_HOST' user failed"
-    }
+    $SSH $NODE_USER@$NODE_HOST 'echo `whoami`@`hostname`' || \
+        die "remote ssh '$NODE_USER@$NODE_HOST' user failed"
+}
+
+
+randpasswd() {
+    echo `(cat /dev/urandom | strings | tr -c -d "a-zA-Z0-9-_" | fold -w 25 | head -1) 2> $NULLFILE`
+}
+
+
+# ----------------------------------------------------------------------
+# delete
+# ----------------------------------------------------------------------
+delete() {
+    # check ssh w/ sudo
+    $SSH $NODE_HOST '$SUDO true' || \
+        die "ssh '$NODE_HOST' sudo failed"
+
+    # force check
+    if [ -z $FORCE ] ; then
+        $SSH $NODE_HOST "grep $Q '^$NODE_USER:' $PASSWDFILE" || \
+            die "remote user '$NODE_USER@$NODE_HOST' does not exist"
+        $SSH $NODE_HOST "grep $Q '^$NODE_USER:' $GROUPFILE" || \
+            die "remote group '$NODE_USER@$NODE_HOST' does not exist"
+        die "-f option is required"
+    fi
+
+    # kill all old user processes
+    $SSH $NODE_HOST "$SUDO pkill -9 -u $NODE_USER &> $NULLFILE || true" || \
+        die "remote user '$NODE_USER@$NODE_HOST' kill failed"
+
+    # delete old user and group
+    $SSH $NODE_HOST "grep $Q '^$NODE_USER:' $PASSWDFILE" && \
+        ($SSH $NODE_HOST "$SUDO $USERDEL -r $NODE_USER" || \
+        die "remote user '$NODE_USER@$NODE_HOST' del failed")
+    $SSH $NODE_HOST "grep $Q '^$NODE_USER:' $GROUPFILE" && \
+        ($SSH $NODE_HOST "$SUDO $GROUPDEL $NODE_USER" || \
+        die "remote group '$NODE_USER@$NODE_HOST' del failed")
+}
