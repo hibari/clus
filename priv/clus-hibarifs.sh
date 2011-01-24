584a17b2c9202056f7a71b7d7ae6b369d5d2c443
Joseph Wayne Norton <norton@alum.mit.edu>
2011-01-24 14:36:07 +0900

    rework instructions and clus-hibarifs.sh script

--- priv/clus-hibarifs.sh
+++ priv/clus-hibarifs.sh
@@ -64,20 +64,21 @@ usage() {
 Usage: $0 [-f] <command> ...
 
   <command> is one of the following:
-    init <user> <hibari.cfg> <hibari-X.Y.Z-DIST-ARCH-WORDSIZE>.tgz
-    start <user> <hibari.cfg>
-    bootstrap <user> <hibari.cfg>
-    stop <user> <hibari.cfg>
+    init <user> <hibarifs.cfg> <hibarifs-X.Y.Z-DIST-ARCH-WORDSIZE>.tgz <hibariuser>
+    start <user> <hibarifs.cfg>
+    bootstrap <user> <hibarifs.cfg> <hibariuser>
+    stop <user> <hibarifs.cfg>
 
   - "-f" is enable force mode.  disable safety checks to prevent
      deleting and/or overwriting an existing installation.
   - <user> is the account on the server(s) where you will be
-    installing Hibari
-  - <hibari.cfg> is Hibari's cluster config file
-
+    installing Hibarifs
+  - <hibarifs.cfg> is Hibarifs's cluster config file
+  - <hibariuser> is the account on the server(s) where Hibari is
+    running
 
 Example usage:
-  $0 init hibari hibari.cfg hibari-X.Y.Z-DIST-ARCH-WORDSIZE.tgz
+  $0 init hibarifs hibarifs.cfg hibarifs-X.Y.Z-DIST-ARCH-WORDSIZE.tgz hibari
 
 Notes:
   - <user> and hosts in the cluster config file must be simple names
@@ -116,87 +117,50 @@ args_sanity() {
     if [[ -z $1 || -z $2 || -z $3 ]] ; then
         usage
     elif [ "$1" = "init" ] ; then
-        if [ -z $4 ] ; then
+        if [[ -z $4 || -z $5 ]] ; then
             usage
         elif [ ! -f "$4" ] ; then
             usage
         else
             TARBALL=$4
+            HIBARI_NODE_USER=$5
+        fi
+    elif [ "$1" = "bootstrap" ] ; then
+        if [ -z "$4" ] ; then
+            usage
+        else
+            HIBARI_NODE_USER=$4
         fi
     fi
 
     CMD=$1
     NODE_USER=$2
     CONFIG_FILE=$3
-    TARBALL=$4
 
     echo $NODE_USER | $PERL -lne 'exit 1 if /[^a-zA-Z0-9._-]/' || \
         die "user '$NODE_USER' invalid"
     if [ "$NODE_USER" = "root" ] ; then
         die "user '$NODE_USER' unsupported"
     fi
+    echo $HIBARI_NODE_USER | $PERL -lne 'exit 1 if /[^a-zA-Z0-9._-]/' || \
+        die "user '$HIBARI_NODE_USER' invalid"
+    if [ "$HIBARI_NODE_USER" = "root" ] ; then
+        die "user '$HIBARI_NODE_USER' unsupported"
+    fi
 
     source $CONFIG_FILE || \
         die "source '$CONFIG_FILE' failed"
 
-    for i in $ADMIN_NODES $BRICK_NODES $ALL_NODES ; do
+    for i in $ADMIN_NODES $CLIENT_NODES ; do
         echo $i | $PERL -lne 'exit 1 if /[^a-zA-Z0-9._-]/' || \
             die "host '$i' invalid"
     done
 
-    if [ ${#ALL_NODES[@]} -lt 1 ] ; then
-        die "missing ALL_NODES in '$CONFIG_FILE'"
-    fi
     if [ ${#ADMIN_NODES[@]} -lt 1 ] ; then
         die "missing ADMIN_NODES in '$CONFIG_FILE'"
     fi
-    if [ ${#BRICK_NODES[@]} -lt 1 ] ; then
-        die "missing BRICK_NODES in '$CONFIG_FILE'"
-    fi
-    if [ -z $BRICKS_PER_CHAIN ] ; then
-        die "missing BRICKS_PER_CHAIN in '$CONFIG_FILE'"
-    fi
-    if [ $BRICKS_PER_CHAIN -lt 1 ] ; then
-        die "invalid BRICKS_PER_CHAIN in '$CONFIG_FILE'"
-    fi
-    if [ ${#ALL_NETA_ADDRS[@]} -lt 1 ] ; then
-        die "missing ALL_NETA_ADDRS in '$CONFIG_FILE'"
-    fi
-    if [ ${#ALL_NETB_ADDRS[@]} -lt 1 ] ; then
-        die "missing ALL_NETB_ADDRS in '$CONFIG_FILE'"
-    fi
-    if [ -z $ALL_NETA_BCAST ] ; then
-        die "missing ALL_NETA_BCAST in '$CONFIG_FILE'"
-    fi
-    if [ -z $ALL_NETB_BCAST ] ; then
-        die "missing ALL_NETB_BCAST in '$CONFIG_FILE'"
-    fi
-    if [ -z $ALL_NETA_TIEBREAKER ] ; then
-        die "missing ALL_NETA_TIEBREAKER in '$CONFIG_FILE'"
-    fi
-    if [ -z $ALL_HEART_UDP_PORT ] ; then
-        die "missing ALL_HEART_UDP_PORT in '$CONFIG_FILE'"
-    fi
-    if [ -z $ALL_HEART_XMIT_UDP_PORT ] ; then
-        die "missing ALL_HEART_XMIT_UDP_PORT in '$CONFIG_FILE'"
-    fi
-
-    if [ ${#ALL_NODES[@]} -lt ${#ADMIN_NODES[@]} ] ; then
-        die "ALL_NODES is less than ADMIN_NODES"
-    fi
-    if [ ${#ADMIN_NODES[@]} -eq 1 ] ; then
-        true
-    elif [ ${#ADMIN_NODES[@]} -eq 2 ] ; then
-        die "2 ADMIN_NODES is not supported"
-    fi
-    if [ ${#ALL_NODES[@]} -lt ${#BRICK_NODES[@]} ] ; then
-        die "ALL_NODES is less than BRICK_NODES"
-    fi
-    if [ ${#ALL_NODES[@]} -ne ${#ALL_NETA_ADDRS[@]} ] ; then
-        die "nonequal # of ALL_NODES and ALL_NETA_ADDRS"
-    fi
-    if [ ${#ALL_NODES[@]} -ne ${#ALL_NETB_ADDRS[@]} ] ; then
-        die "nonequal # of ALL_NODES and ALL_NETB_ADDRS"
+    if [ ${#CLIENT_NODES[@]} -lt 1 ] ; then
+        die "missing CLIENT_NODES in '$CONFIG_FILE'"
     fi
 }
 
@@ -205,64 +169,43 @@ args_sanity() {
 # init
 # ----------------------------------------------------------------------
 init() {
-    local N0=${#ALL_NODES[@]}
+    local N0=${#CLIENT_NODES[@]}
     local N=$(($N0 - 1))
 
-    # generate cookie
-    local COOKIE=`randcookie`
+    # fetch cookie
+    local COOKIE=`fetchcookie`
     if [ -z $COOKIE ] ; then
-        die "generate cookie failed"
+        die "fetch cookie failed"
     fi
 
     # setup NODE
     for I in `seq 0 $N`; do
         (
-            local NODE=${ALL_NODES[$I]}
-            local ADMIN_NODES_MINUSMOI=(`echo ${ADMIN_NODES[@]} | xargs -n 1 echo | grep -v $NODE | xargs echo`)
+            local NODE=${CLIENT_NODES[$I]}
+            local CS_NODE=(`hibarifs_nodes_cs $NODE`)
 
-            local CS_NODE=(`hibari_nodes_cs $NODE`)
-            local CS_ALL_NODES=(`hibari_nodes_cs ${ALL_NODES[@]}`)
-            local CS_ADMIN_NODES=(`hibari_nodes_cs ${ADMIN_NODES[@]}`)
-            local CS_ADMIN_NODES_MINUSMOI=(`hibari_nodes_cs ${ADMIN_NODES_MINUSMOI[@]}`)
-
-            # stop Hibari
-            $SSH $NODE_USER@$NODE "(source .bashrc; cd hibari &> $NULLFILE; ./bin/hibari stop &> $NULLFILE) || true" || \
+            # stop Hibarifs
+            $SSH $NODE_USER@$NODE "(source .bashrc; cd hibarifs &> $NULLFILE; ./bin/hibarifs stop &> $NULLFILE) || true" || \
                 die "node $NODE stop failed"
-            # kill all Hibari beam.smp processes
+            # kill all Hibarifs beam.smp processes
             $SSH $NODE_USER@$NODE "pkill -9 -u $NODE_USER beam.smp || true" || \
                 die "node $NODE pkill beam.smp failed"
 
-            # scp Hibari tarball
+            # scp Hibarifs tarball
             $SCP $TARBALL $NODE_USER@$NODE:$TARBALL || \
                 die "node $NODE scp tarball failed"
 
-            # untar Hibari package
-            $SSH $NODE_USER@$NODE "rm -rf hibari; tar -xzf $TARBALL" || \
+            # untar Hibarifs package
+            $SSH $NODE_USER@$NODE "rm -rf hibarifs; tar -xzf $TARBALL" || \
                 die "node $NODE untar tarball failed"
 
-            # configure Hibari package
+            # configure Hibarifs package
             $SSH $NODE_USER@$NODE "sed -i.bak \
 -e 's/-sname .*/-sname $CS_NODE/' \
 -e 's/-name .*/-sname $CS_NODE/' \
 -e 's/-setcookie .*/-setcookie $COOKIE/' \
-hibari/etc/vm.args" || \
+hibarifs/etc/vm.args" || \
                 die "node $NODE vm.args setup failed"
-
-            $SSH $NODE_USER@$NODE "sed -i.bak \
--e 's/{distributed, \[{gdss_admin, \(.*\), .*}\]}/{distributed, \[{gdss_admin, \1, [$CS_ADMIN_NODES]}\]}/' \
--e 's/{sync_nodes_optional,.*}/{sync_nodes_optional, [$CS_ADMIN_NODES_MINUSMOI]}/' \
--e 's/{admin_server_distributed_nodes,.*}/{admin_server_distributed_nodes, [$CS_ADMIN_NODES]}/' \
--e 's/{network_a_address,.*}/{network_a_address, \"${ALL_NETA_ADDRS[$I]}\"}/' \
--e 's/{network_b_address,.*}/{network_b_address, \"${ALL_NETB_ADDRS[$I]}\"}/' \
--e 's/{network_a_broadcast_address,.*}/{network_a_broadcast_address, \"$ALL_NETA_BCAST\"}/' \
--e 's/{network_b_broadcast_address,.*}/{network_b_broadcast_address, \"$ALL_NETB_BCAST\"}/' \
--e 's/{network_a_tiebreaker,.*}/{network_a_tiebreaker, \"$ALL_NETA_TIEBREAKER\"}/' \
--e 's/{network_monitor_enable,.*}/{network_monitor_enable, true}/' \
--e 's/{network_monitor_monitored_nodes,.*}/{network_monitor_monitored_nodes, [$CS_ALL_NODES]}/' \
--e 's/{heartbeat_status_udp_port,.*}/{heartbeat_status_udp_port, $ALL_HEART_UDP_PORT}/' \
--e 's/{heartbeat_status_xmit_udp_port,.*}/{heartbeat_status_xmit_udp_port, $ALL_HEART_XMIT_UDP_PORT}/' \
-hibari/etc/app.config" || \
-                die "node $NODE app.config setup failed"
 
             echo $NODE_USER@$NODE
         ) &
@@ -271,8 +214,10 @@ hibari/etc/app.config" || \
     wait
 }
 
-randcookie() {
-    echo `(cat /dev/urandom | strings | tr -c -d "a-zA-Z0-9-_" | fold -w 25 | head -1) 2> $NULLFILE`
+fetchcookie() {
+    local NODE=${ADMIN_NODES[0]}
+    $SSH $HIBARI_NODE_USER@$NODE "grep setcookie hibari/etc/vm.args | xargs -n 1 echo | grep -v setcookie" || \
+        die "node $NODE vm.args setup failed"
 }
 
 
@@ -280,16 +225,16 @@ randcookie() {
 # start
 # ----------------------------------------------------------------------
 start() {
-    local N0=${#ALL_NODES[@]}
+    local N0=${#CLIENT_NODES[@]}
     local N=$(($N0 - 1))
 
     # start NODE
     for I in `seq 0 $N`; do
         (
-            local NODE=${ALL_NODES[$I]}
+            local NODE=${CLIENT_NODES[$I]}
 
-            # start Hibari package
-            $SSH $NODE_USER@$NODE "source .bashrc; cd hibari; ./bin/hibari start" || \
+            # start Hibarifs package
+            $SSH $NODE_USER@$NODE "source .bashrc; cd hibarifs; ./bin/hibarifs start" || \
                 die "node $NODE start failed"
 
             echo $NODE_USER@$NODE
@@ -304,14 +249,14 @@ start() {
 # bootstrap
 # ----------------------------------------------------------------------
 bootstrap() {
-    local NODE=${ADMIN_NODES[0]}
-    local WS_BRICK_NODES=`hibari_nodes_ws ${BRICK_NODES[@]}`
+    local ADMIN_NODE=${ADMIN_NODES[0]}
+    local WS_NODES=`hibarifs_nodes_ws ${CLIENT_NODES[@]}`
 
-    # bootstrap Hibari package
-    $SSH $NODE_USER@$NODE "source .bashrc; cd hibari; ./bin/hibari-admin bootstrap $BRICKS_PER_CHAIN $WS_BRICK_NODES" || \
+    # bootstrap Hibarifs package
+    $SSH $HIBARI_NODE_USER@$ADMIN_NODE "source .bashrc; cd hibari; ./bin/hibari-admin client-add $WS_NODES" || \
         die "node $NODE bootstrap failed"
-
-    echo "$NODE_USER@$NODE => $WS_BRICK_NODES"
+    $SSH $HIBARI_NODE_USER@$ADMIN_NODE "source .bashrc; cd hibari; ./bin/hibari-admin client-list" || \
+        die "node $NODE bootstrap failed"
 }
 
 
@@ -319,16 +264,16 @@ bootstrap() {
 # stop
 # ----------------------------------------------------------------------
 stop() {
-    local N0=${#ALL_NODES[@]}
+    local N0=${#CLIENT_NODES[@]}
     local N=$(($N0 - 1))
 
     # stop NODE
     for I in `seq 0 $N`; do
         (
-            local NODE=${ALL_NODES[$I]}
+            local NODE=${CLIENT_NODES[$I]}
 
-            # stop Hibari package
-            $SSH $NODE_USER@$NODE "source .bashrc; cd hibari; ./bin/hibari stop" || \
+            # stop Hibarifs package
+            $SSH $NODE_USER@$NODE "source .bashrc; cd hibarifs; ./bin/hibarifs stop" || \
                 die "node $NODE stop failed"
 
             echo $NODE_USER@$NODE
@@ -340,9 +285,9 @@ stop() {
 
 
 # ----------------------------------------------------------------------
-# hibari_nodes_cs
+# hibarifs_nodes_cs
 # ----------------------------------------------------------------------
-hibari_nodes_cs() {
+hibarifs_nodes_cs() {
     local NODES=($@)
     local N0=${#NODES[@]}
 
@@ -352,7 +297,7 @@ hibari_nodes_cs() {
 
         for X in `seq 0 $N`; do
             local node=`echo ${NODES[$X]} | sed "s/'//g"`
-            nodes="$nodes,hibari@$node"
+            nodes="$nodes,hibarifs@$node"
         done
         echo `echo $nodes | sed 's/^,//'`
     else
@@ -362,9 +307,9 @@ hibari_nodes_cs() {
 
 
 # ----------------------------------------------------------------------
-# hibari_nodes_ws
+# hibarifs_nodes_ws
 # ----------------------------------------------------------------------
-hibari_nodes_ws() {
+hibarifs_nodes_ws() {
     local NODES=($@)
     local N0=${#NODES[@]}
 
@@ -374,7 +319,7 @@ hibari_nodes_ws() {
 
         for X in `seq 0 $N`; do
             local node=`echo ${NODES[$X]} | sed "s/'//g"`
-            nodes="$nodes hibari@$node"
+            nodes="$nodes hibarifs@$node"
         done
         echo `echo $nodes | sed 's/^ //'`
     else
